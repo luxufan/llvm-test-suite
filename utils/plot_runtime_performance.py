c4657bd181b98a81ebeb0f57cc0b077328c1f8d2
luxufan <luxufan981014@gmail.com>
2024-09-13 01:34:24 +0800

    change plot_runtime_performance.py

--- utils/plot_runtime_performance.py
+++ utils/plot_runtime_performance.py
@@ -40,31 +40,51 @@ def main():
     full_improvement = full_improvement.rename(columns={"exec_time" : "fulllto"})
     thin_improvement = thin_improvement.rename(columns={"exec_time" : "thinlto"})
 
-    width = 0.25
-    multiplier = 0
-    plt.rcParams.update({'font.size': 40})
-    fig, ax = plt.subplots()
-    fig.set_figheight(30)
-    fig.set_figwidth(46)
     improvement = thin_improvement
     improvement.insert(1, "fulllto", full_improvement["fulllto"], allow_duplicates=True)
     improvement = improvement.round(2)
     improvement = improvement.drop(["chrome"])
 
+    print(improvement)
+
     ## deal with envoy
     improvement.iloc[0] = - improvement.iloc[0]
+
+    width = 0.35
+    multiplier = 0
+    plt.rcParams.update({'font.size': 30})
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    fig.subplots_adjust(hspace=0.08)
+
+    ax1.spines['bottom'].set_visible(False)
+    ax2.spines['top'].set_visible(False)
+    ax1.xaxis.tick_top()
+    ax1.tick_params(labeltop=False)
+    ax2.xaxis.tick_bottom()
+
+    fig.set_figheight(20)
+    fig.set_figwidth(30)
     x = np.arange(len(improvement.index))
+    kwargs = dict(linewidth=0.02, visible=True)
     for col in improvement.columns:
         offset = width * multiplier
-        rects = ax.bar(x + offset, improvement[col], width, label=col)
-        ax.bar_label(rects, padding = 3)
+        rects = ax1.bar(x + offset, improvement[col], width, clip_on=False, label=col, **kwargs)
+        ax2.bar(x + offset, improvement[col], width, label=col, **kwargs)
+        #ax1.bar_label(rects, padding = 3)
         multiplier += 1
-    ax.set_ylim(auto=True)
-    ax.set_ylabel('performance improvement percentage')
-    ax.set_xlabel("benchmarks")
-    ax.set_xticks(x + width, improvement.index)
-    ax.legend(loc="upper right", ncols=2, fontsize="large")
+
+    ax1.set_ylim(50, 100)
+    ax2.set_ylim(0, 3)
+    ax2.set_ylabel('performance improvement percentage')
+    ax2.set_xlabel("benchmarks")
+    ax2.set_xticks(x + width/2, improvement.index)
+    ax1.legend(loc="upper right", ncols=2, fontsize="xx-large")
     #plot = base_improvement.plot.bar(rot=0, figsize=(46, 30))
+    d = .5
+    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=60,
+              linestyle="none", color='k', mec='k', mew=1, clip_on=False)
+    ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
+    ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
     plt.show()
     plt.savefig(metrics[0].replace(".", "_"))
 
