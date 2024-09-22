fix buffer overflow

utils/plot_runtime_performance.py
@@ -53,7 +53,7 @@ def main():
     width = 0.35
     multiplier = 0
     plt.rcParams.update({'font.size': 15})
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 6))
     fig.subplots_adjust(hspace=0.08)
 
     ax1.spines['bottom'].set_visible(False)
@@ -62,8 +62,6 @@ def main():
     ax1.tick_params(labeltop=False)
     ax2.xaxis.tick_bottom()
 
-    fig.set_figheight(10)
-    fig.set_figwidth(15)
     x = np.arange(len(improvement.index))
     kwargs = dict(linewidth=0.02, visible=True)
     for col in improvement.columns:
@@ -75,14 +73,14 @@ def main():
 
     ax1.set_ylim(40, 80)
     ax2.set_ylim(-2, 3)
-    ax2.set_ylabel('performance improvement percentage')
+    ax2.set_ylabel('performance improvement percentage', loc='bottom')
     ax2.set_xlabel("benchmarks")
     ax2.set_xticks(x + width/2, improvement.index)
-    ax1.legend(loc="upper right", ncols=2, fontsize="xx-large")
+    ax1.legend(loc="upper right", ncols=2, fontsize="20")
     #plot = base_improvement.plot.bar(rot=0, figsize=(46, 30))
     d = .5
     leg = plt.legend()
-    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=32,
+    kwargs = dict(marker=[(-1, -d), (1, d)], markersize=28,
               linestyle="none", color='k', mec='k', mew=3, clip_on=False)
     ax1.plot([0, 1], [0, 0], transform=ax1.transAxes, **kwargs)
     ax1.plot([0, 0.303], [0, 0], transform=ax1.transAxes, **kwargs)
@@ -90,6 +88,7 @@ def main():
     ax2.plot([0, 1], [1, 1], transform=ax2.transAxes, **kwargs)
     ax2.plot([0.303, 1], [1, 1], transform=ax2.transAxes, **kwargs)
     ax2.plot([0.345, 1], [1, 1], transform=ax2.transAxes, **kwargs)
+    ax2.get_legend().remove()
     plt.show()
     plt.savefig(metrics[0].replace(".", "_"))
 