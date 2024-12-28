be000d9e21cc594565f1e48eb2b1506cd6fec793
luxufan <luxufan981014@gmail.com>
2024-12-28 22:56:48 +0800

    add plot_code_size.py

--- utils/plot_code_size.py
+++ utils/plot_code_size.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
+import sys
 
 def main():
     parser = argparse.ArgumentParser(prog="plot_runtime_performance.py")
@@ -18,12 +19,23 @@ def main():
                         help="Base fulllto file")
     parser.add_argument("compare_full",
                         help="Compare fulllto file")
+
+    rename_map = {'blender': 'Blender', 'chrome': 'Chromium', 'povray': 'POV-Ray', 'solc':'Solidity', 'envoy-static':'Envoy', 'opt':'LLVM', '471.omnetpp':'OMNeT++', '447.dealII':'deal.II', 'd8':'V8', 'z3':'Z3'}
+    rows = ['Blender', 'Chromium', 'deal.II', 'Envoy', 'LLVM', 'OMNeT++', 'POV-Ray', 'Solidity', 'V8', 'Z3']
     config = parser.parse_args()
     base_thin_data = read(config.base_thin)
+    base_thin_data.rename(index=rename_map, inplace=True)
+    base_thin_data = base_thin_data.reindex(rows)
     base_full_data = read(config.base_full)
+    base_full_data.rename(index=rename_map, inplace=True)
+    base_full_data = base_full_data.reindex(rows)
 
     compare_thin_data = read(config.compare_thin)
+    compare_thin_data.rename(index=rename_map, inplace=True)
+    compare_thin_data = compare_thin_data.reindex(rows)
     compare_full_data = read(config.compare_full)
+    compare_full_data.rename(index=rename_map, inplace=True)
+    compare_full_data = compare_full_data.reindex(rows)
 
     # Extract metrics
     metrics = config.metrics
@@ -37,42 +49,53 @@ def main():
     compare_full_metrics = compare_full_data[metrics]
     thin_improvement = ((base_thin_metrics - compare_thin_metrics) / base_thin_metrics) * 100
     full_improvement = ((base_full_metrics - compare_full_metrics) / base_full_metrics) * 100
-    full_improvement = full_improvement.rename(columns={"size" : "fulllto"})
-    thin_improvement = thin_improvement.rename(columns={"size" : "thinlto"})
+    full_improvement = full_improvement.rename(columns={"size" : "LTO"})
+    thin_improvement = thin_improvement.rename(columns={"size" : "ThinLTO"})
 
-    improvement = thin_improvement
-    improvement.insert(1, "fulllto", full_improvement["fulllto"], allow_duplicates=True)
+    improvement = full_improvement
+    improvement.insert(1, "ThinLTO", thin_improvement["ThinLTO"], allow_duplicates=True)
     improvement = improvement.round(2)
 
     print(improvement)
 
-    width = 0.35
+    colors = {'LTO': 'xkcd:azure', 'ThinLTO': 'tab:orange'}
+    edgecolors = {'LTO': 'black', 'ThinLTO': 'black'}
+    width = 0.3
     multiplier = 0
-    plt.rcParams.update({'font.size': 13})
-    fig, ax1 = plt.subplots(figsize=(10, 6))
-    fig.subplots_adjust(hspace=0.08)
+    plt.rcParams.update({'font.size': 18})
+    fig, ax1 = plt.subplots(figsize=(13, 6.7))
+    fig.subplots_adjust(hspace=0.08, bottom=0.2)
 
-    ax1.set_title("code size")
+    for spine in ax1.spines.values():
+        spine.set_color('xkcd:almost black')
+        spine.set_linewidth(1.5)
+
+    ax1.set_title("Code Size")
     x = np.arange(len(improvement.index))
-    kwargs = dict(linewidth=0.02, visible=True)
+    kwargs = dict(linewidth=1.8, edgecolor='black', visible=True)
     for col in improvement.columns:
-        offset = width * multiplier
+        offset = (width) * multiplier
         labels = improvement[col].round(2).astype('str') + '%'
-        rects = ax1.bar(x + offset, improvement[col], width, label=col, **kwargs)
+        rects = ax1.bar(x + offset, improvement[col], width, label=col, linewidth=1.5, color=colors[col], edgecolor=edgecolors[col])
         #ax1.bar_label(rects, labels=labels, padding=3)
         #ax2.bar(x + offset, improvement[col], width, label=col, **kwargs)
         #ax1.bar_label(rects, padding = 3)
         multiplier += 1
 
-    ax1.set_ylim(-1, 6)
-    ax1.set_ylabel('code size')
-    ax1.set_xlabel("benchmarks")
-    ax1.set_xticks(x + width/2, improvement.index)
+    fontsize = 20
+    ax1.set_ylim(0, 6)
+    ax1.set_ylabel('Size Reduction', fontsize=fontsize)
+    ax1.set_xticks(x + width/2, improvement.index, fontsize=fontsize)
+    ax1.tick_params(axis='x', labelrotation=20)
+    # add percentage symbol to y ticks
+    vals = ax1.get_yticks()
+    ax1.set_yticklabels(['{0}%'.format(round(x)) for x in vals], fontsize=fontsize)
+
     ax1.legend(loc="upper right", ncols=2, fontsize="20")
     #plot = base_improvement.plot.bar(rot=0, figsize=(46, 30))
     d = .5
     plt.show()
-    plt.savefig(metrics[0].replace(".", "_"))
+    plt.savefig(metrics[0].replace(".", "_") + ".pdf")
 
 if __name__ == "__main__":
     main()
